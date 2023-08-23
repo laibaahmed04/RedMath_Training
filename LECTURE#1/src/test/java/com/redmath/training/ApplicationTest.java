04be1ee6f0101551646727168a72cffca10dfec4
DESKTOP-P4QH9DC\laiba.ahmad <laibaahmed04@gmail.com>
2023-08-23 15:08:34 +0500

    1st Lecture

---
+++ LECTURE#1/src/test/java/com/redmath/training/ApplicationTest.java
@@ -0,0 +1,27 @@
+package com.redmath.training;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApplicationTest{
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHello() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/index.html"))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
+                .andExpect(MockMvcResultMatchers.content().string(
+                        Matchers.startsWith("<h1>Hello World</h1>")));
+    }
+}
